50204794c2c37b9cc918e0706024908b6829a453
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-24 14:04:07 +0800

    chunk

---
+++ Chunk Array.js
@@ -0,0 +1,11 @@
+var chunk = function (arr, size) {
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {}
+};
+
+console.log(chunk([1, 9, 6, 3, 2], 1));
