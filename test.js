91f5108a71df776a0987b012b234ce028379dc40
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-05-27 10:13:24 +0800

    fnComposition

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-const arrs = [1, 2];
+const arr = [1, 2, 3, 4, -5];
 
-console.log(arrs.slice(0, 5));
+console.log(arr.reduce((prev, curr) => prev + curr));
+console.log(arr.reduceRight((prev, curr) => prev + curr));
